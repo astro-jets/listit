6be26d1afaa415ce64e59452682be56e0e868e54
astro-jets <mphatsomtogolo30@gmail.com>
2026-03-08 21:49:12 +0200

    intial user mvp

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("pages/public/Dashboard.tsx"),
   route("/profile", "pages/public/Profile.tsx"),
-  route("/messages", "pages/public/Mssages.tsx"),
+  route("/messages", "pages/public/Messages.tsx"),
   route("/myshop", "pages/public/MyShop.tsx"),
   route("/analytics", "pages/public/Analytics.tsx"),
 ] satisfies RouteConfig;
