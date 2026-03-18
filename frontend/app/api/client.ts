742685bba09d6eab47bf672a81063f99f983b567
astro-jets <mphatsomtogolo30@gmail.com>
2026-03-18 13:12:41 +0200

    Changed API connection settings

--- frontend/app/api/client.ts
+++ frontend/app/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.API_URL||'https://listit-c5yb.onrender.com/'
+const API_BASE = import.meta.env.API_URL || "https://listit-c5yb.onrender.com/";
 
 export const client = axios.create({
   baseURL: API_BASE,
