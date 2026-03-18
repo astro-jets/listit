8a945923baa57a1de1ebd614dc08056169ca847f
astro-jets <mphatsomtogolo30@gmail.com>
2026-03-18 13:26:49 +0200

    CORS

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -3,7 +3,7 @@ import cors from "@fastify/cors";
 
 export default fp(async (app) => {
   app.register(cors, {
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "listit-ten.vercel.app"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"], // MUST include DELETE
     allowedHeaders: ["Content-Type", "Authorization"],
   });
